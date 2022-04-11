e6d25dba6ac28a96079584de730611ce1080567f
tk <tk@renard.ninja>
2022-04-11 02:07:50 +0200

    ajoute le calcul de la progression entre 2017 et 2022

--- présidentielle-2022/différence.py
+++ présidentielle-2022/différence.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -17,13 +18,15 @@
 import pandas as pd
 
 # %%
-pd.set_option("display.float_format", lambda x: '{:,.0f}'.format(x).replace(",", " "))
+pd.set_option("display.float_format", lambda x: '{:,.2f}'.format(x).replace(",", " "))
 
 # %%
 df = (pd
-    .read_html("https://www.resultats-elections.interieur.gouv.fr/presidentielle-2022/FE.html",
+    .read_html(
+        "https://www.resultats-elections.interieur.gouv.fr/presidentielle-2022/FE.html",
         thousands=" ",
-        decimal=",")
+        decimal=",",
+    )
     [1]
     .assign(
         diff = lambda df_: (df_["Voix"]
@@ -34,5 +37,36 @@ df = (pd
 )
 
 df
+
+# %%
+df.dtypes
+
+# %%
+url_2017 = "https://www.interieur.gouv.fr/Elections/Les-resultats/Presidentielles/elecresult__presidentielle-2017/(path)/presidentielle-2017//FE.html"
+
+df_2017 = (pd
+    .read_html(url_2017, encoding="utf-8",
+        thousands=" ",
+        decimal=",",)
+    [2]
+)
+
+df_2017
+
+# %%
+(pd
+    .merge(df, df_2017, on="Liste des candidats", suffixes=('_2022', '_2017'))
+    [["Liste des candidats", "Voix_2017", "Voix_2022"]]
+    .assign(
+        difference = lambda df_: df_["Voix_2022"] - df_["Voix_2017"],
+        pct = lambda df_: df_["Voix_2022"] / df_["Voix_2017"] - 1
+    )
+    .set_index("Liste des candidats")
+    .style
+        .format(thousands=" ",
+            formatter={'pct': '{:,.2%}'.format}
+#            formatter = {('difference'): lambda x: "🔼 "+str(x) if x > 0 else "🔽 "+str(x) }
+        )
+)
 
 # %%
